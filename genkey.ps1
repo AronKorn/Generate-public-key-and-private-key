c941c2fa95f0f68e46192ae0e44be419aae5c727
Aron Kornbluh <104830414+AronKorn@users.noreply.github.com>
2022-05-24 12:17:32 +0300

    Update genkey.ps1

--- genkey.ps1
+++ genkey.ps1
@@ -1,2 +1,5 @@
-﻿openssl genrsa -out privatekey.pem 2048
+# Defines a private key named privatekey.pem
+openssl genrsa -out privatekey.pem 2048
+
+# Defines a public key named publickey.crt from the private key
 openssl rsa -in privatekey.pem -pubout -out publickey.crt
